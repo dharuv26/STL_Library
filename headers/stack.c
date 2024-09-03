c0182ae71de4eff682a1621616f73d7b2a04a8a8
Dharuv Singla <dharuv268.be22@chittkara.edu.in>
2024-09-04 01:22:37 +0530

    stack codes added

---
+++ headers/stack.c
@@ -0,0 +1,87 @@
+#include "stack.h"
+
+//-----------------Stack Node definition------------------------
+
+struct sNode {
+	int data;
+	struct sNode* next;
+};
+
+//-----------------Stack definition-----------------------------
+
+struct stack {
+	sNode* top;
+	int size;
+};
+
+//-----------------Stack Initialization-------------------------
+
+stack* createStack() {
+	stack* stack = (stack*)malloc(sizeof(stack));
+	stack->top = NULL;
+	stack->size = 0;
+	return stack;
+}
+
+//-----------------Stack Node Initialization--------------------
+
+sNode* createNode(int data) {
+	sNode* newNode = (sNode*)malloc(sizeof(sNode));
+	newNode->data = data;
+	newNode->next = NULL;
+	return newNode;
+}
+
+//-----------------Push element to stack------------------------
+
+void spush(stack* stack, int data) {
+	sNode* newNode = createNode(data);
+	newNode->next = stack->top;
+	stack->top = newNode;
+	stack->size++;
+}
+
+//-----------------Pop element from stack-----------------------
+
+void spop(stack* stack) {
+	if (stack->top == NULL) {
+		printf("Stack is empty.\n");
+		return;
+	}
+	sNode* temp = stack->top;
+	stack->top = stack->top->next;
+	free(temp);
+	stack->size--;
+}
+
+//-----------------return element on top of stack---------------
+
+int stop(stack* stack) {
+	if (stack->top == NULL) {
+		printf("Stack is empty.\n");
+		return -1;
+	}
+	return stack->top->data;
+}
+
+//-----------------get size of stack----------------------------
+
+int sgetSize(stack* stack) {
+	return stack->size;
+}
+
+//-----------------Check if stack is empty----------------------
+
+bool sisEmpty(stack* stack) {
+	return stack->size == 0;
+}
+
+//-----------------Delete stack---------------------------------
+
+void sdeleteStack(stack* stack) {
+	while (!sisEmpty(stack)) {
+		spop(stack);
+	}
+	free(stack);
+}
+
---
+++ headers/stack.h
@@ -0,0 +1,20 @@
+#ifndef stack_h
+#define stack_h
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct sNode sNode;
+typedef struct stack stack;
+
+stack* createStack();
+sNode* createNode(int data);
+void spush(stack* stack, int data);
+void spop(stack* stack);
+int stop(stack* stack);
+int sgetSize(stack* stack);
+bool sisEmpty(stack* stack);
+void deleteStack(stack* stack);
+
+#endif
